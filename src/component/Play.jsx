3588b7abb14721bc4d633ccbf917418d4669084e
StevenDomagala <steven.domagala@gmail.com>
2023-12-09 13:49:45 +1100

    Create JPG slider for Play Page

--- src/component/Play.jsx
+++ src/component/Play.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Play = () => {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="flex flex-col justify-between max-w-xl px-4 mx-auto lg:pt-16 lg:flex-row md:px-8 lg:max-w-screen-xl" style={{margin: 0}}>
       <div className="mb-16 lg:mb-0 lg:max-w-lg lg:pr-5">
+        <Slider {...sliderSettings}>
+          <div>
+            <img src="images/play/1.jpg" alt="Image 1" />
+          </div>
+          <div>
+            <img src="images/play/2.jpg" alt="Image 1" />
+          </div>
+          <div>
+            <img src="images/play/3.jpg" alt="Image 1" />
+          </div>
+          <div>
+            <img src="images/play/4.jpg" alt="Image 1" />
+          </div>
+          <div>
+            <img src="images/play/5.jpg" alt="Image 1" />
+          </div>
+          {/* Add more divs with images as needed */}
+        </Slider>
         <div className="max-w-xl mb-6 color-gray">
           <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight sm:text-4xl sm:leading-none">
-          Let's Play Tennis
+            Let's Play Tennis
           </h2>
-          <h2></h2>
           <div className="small-font-20">
-        gldfjg l dsfglkjsdlfgkj fgdlldfgk jldsfg j<br/>
-        gldfjg l dsfglkjsdlfgkj fgdlldfgk jldsfg j<br/>
-        <br/>
-        gfdhs  gfdkhhgkjds fdgk dgfkkhdsfg gdfkh<br/>
-        gldfjg l dsfglkjsdlfgkj fgdlldfgk jldsfg j<br/>
-        <br/>
-        gldfjg l dsfglkjsdlfgkj fgdlldfgk jldsfg j
-        </div>
+            gldfjg l dsfglkjsdlfgkj fgdlldfgk jldsfg j<br />
+            gldfjg l dsfglkjsdlfgkj fgdlldfgk jldsfg j
+            {/* Add more content as needed */}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Play
+export default Play;
