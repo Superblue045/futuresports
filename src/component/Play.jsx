cda2cdf5c15edcda4f49f3f003a8bfa833a456ff
StevenDomagala <steven.domagala@gmail.com>
2023-12-09 13:59:49 +1100

    update formatting to be centred

--- src/component/Play.jsx
+++ src/component/Play.jsx
@@ -13,27 +13,28 @@ const Play = () => {
   };
 
   return (
-    <div className="flex flex-col justify-between max-w-xl px-4 mx-auto lg:pt-16 lg:flex-row md:px-8 lg:max-w-screen-xl" style={{margin: 0}}>
+    <div className="flex flex-col items-center justify-center max-w-xl mx-auto lg:pt-16 lg:flex-row md:px-8 lg:max-w-screen-xl">
       <div className="mb-16 lg:mb-0 lg:max-w-lg lg:pr-5">
-        <Slider {...sliderSettings}>
+        <Slider {...sliderSettings} className="w-full">
           <div>
-            <img src="images/play/1.jpg" alt="Image 1" />
+            <img src="images/play/1.jpg" alt="Image 1" className="w-full" />
           </div>
           <div>
-            <img src="images/play/2.jpg" alt="Image 1" />
+            <img src="images/play/2.jpg" alt="Image 2" className="w-full" />
           </div>
           <div>
-            <img src="images/play/3.jpg" alt="Image 1" />
+            <img src="images/play/3.jpg" alt="Image 3" className="w-full" />
           </div>
           <div>
-            <img src="images/play/4.jpg" alt="Image 1" />
+            <img src="images/play/4.jpg" alt="Image 4" className="w-full" />
           </div>
           <div>
-            <img src="images/play/5.jpg" alt="Image 1" />
+            <img src="images/play/5.jpg" alt="Image 5" className="w-full" />
           </div>
           {/* Add more divs with images as needed */}
         </Slider>
-        <div className="max-w-xl mb-6 color-gray">
+        <div className="max-w-xl mb-6 color-gray text-center">
+          
           <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight sm:text-4xl sm:leading-none">
             Let's Play Tennis
           </h2>
