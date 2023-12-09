a5a80d86500af1cdd5c4742a812c8549f8bdfd8b
StevenDomagala <steven.domagala@gmail.com>
2023-12-09 14:04:47 +1100

    Update with Sample array of events

--- src/component/Calendar.jsx
+++ src/component/Calendar.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 const Calendar = () => {
+  // Sample array of events
+  const events = [
+    {
+      name: 'Event 1',
+      date: '2023-12-15',
+      description: 'Description for Event 1',
+    },
+    {
+      name: 'Event 2',
+      date: '2023-12-20',
+      description: 'Description for Event 2',
+    },
+    // Add more events as needed
+  ];
+
   return (
-    <div className="px-4 mx-auto lg:pt-16 lg:flex-row md:px-8" style={{margin: 0}}>
-      <div className="color-gray">
-      <h2 className="font-sans text-3xl font-bold tracking-tight sm:text-4xl sm:leading-none" style={{marginBottom: "20px"}}>
-      Calendar of Events
+    <div className="flex items-center justify-center px-4 mx-auto lg:pt-16 lg:flex-row md:px-8" style={{ height: '50vh', margin: 0 }}>
+      <div className="color-gray text-center">
+        <h2 className="font-sans text-3xl font-bold tracking-tight sm:text-4xl sm:leading-none" style={{ marginTop: "20px" }}>
+          Calendar of Events
         </h2>
-        <img src="images/calendar-23NOV.jpg" alt="2023 Nov" />
+        {events.map((event, index) => (
+          <div key={index} className="mb-4">
+            <h3 className="text-xl font-semibold">{event.name}</h3>
+            <p className="text-gray-600">{event.date}</p>
+            <p>{event.description}</p>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
